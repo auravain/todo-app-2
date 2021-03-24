b92feeee3bf9b7354bfc897b8e9915f6fb4d87fd
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-03-25 02:48:57 +0300

    added delete button

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,8 +2,15 @@ import React from "react";
 import "./TodoItem.css";
 
 function Todo(props) {
-  const { content } = props;
-  return <div>{content}</div>;
+  const { content, id } = props;
+	return (
+		<div>
+			{content}
+			<button className="destroy"
+				onClick={() => props.onDelete(id)}>
+				Sil
+			</button>
+		</div>);
 }
 
 export default Todo;
