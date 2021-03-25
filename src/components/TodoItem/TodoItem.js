1c79d014c30c3ccf7208012e2e9160baa2bc4d83
İlknur SUCAKLI <ilknurr.94@hotmail.com>
2021-03-25 04:01:00 +0300

    added strikethrough text function

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './TodoItem.css';
+import { useState } from 'react';
 
 function Todo(props) {
 	const { content, id } = props;
+	const [isDone, setIsDone] = useState(false);
+	const styles = {
+		textDecoration: isDone ? 'line-through' : '',
+		textDecorationColor: isDone ? '#93ADAD' : '',
+	};
+	const toggleIsDone = () => setIsDone(!isDone);
 	return (
-		<div className="content-todos">
+		<div className="content-todos" style={styles} onClick={toggleIsDone}>
 			{content}
 			<button className="destroy" onClick={() => props.onDelete(id)}>
 				Sil
 			</button>
 		</div>
 	);
-};
+}
 
 export default Todo;
